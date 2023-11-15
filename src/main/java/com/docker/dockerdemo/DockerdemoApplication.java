dd9df22c6189005ab5630f1e5106b5a5b3305acd
vinaykumarpa <vinaykumarpa@10.4.33.14>
2023-11-15 14:55:51 +0530

    Commit2

--- src/main/java/com/docker/dockerdemo/DockerdemoApplication.java
+++ src/main/java/com/docker/dockerdemo/DockerdemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerdemoApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerdemoApplication.class, args);
 	}
